01f771688a848b41b6f6be70166de0959ddd8bc1
Teddy Wijaya <teddy_wijaya93@rocketmail.com>
2024-08-19 14:34:43 +0700

    update persembahan

--- page-persembahan.php
+++ page-persembahan.php
@@ -26,6 +26,9 @@
                 $the_query = new WP_Query( $args );
                 while ( $the_query->have_posts() ) : $the_query->the_post();
                 $idPersembahan = get_the_ID();
+                $confirmation = get_post_meta($idPersembahan,'waconfirmation',true);
+                $wapersembahan = $confirmation[0]['wa-persembahan'];
+                $wapersembahanconf = $confirmation[0]['wa-persembahan-confirmation'];
 
                 if($idPersembahan != 25){
             ?>
@@ -33,7 +36,7 @@
                 <div class="bg-persembahan-type" style="background-image:url(<?php echo get_the_post_thumbnail_url($post->ID, 'full'); ?>)"> 
                     <h4 class="title-single-persembahan text-center mb-3"><?php echo get_the_title($idPersembahan); ?></h4>
                     <div class="desc-single-persembahan text-white text-center mb-3"><?php echo get_the_content($idPersembahan); ?></div>
-                    <div class="text-center"><a href="" role="button" class="btn btn-konfirmasi-persembahan">Konfirmasi<br/><?php echo get_the_title($idPersembahan); ?></a></div>
+                    <div class="text-center"><a href="https://wa.me/<?php echo $wapersembahanconf; ?>" target="_blank" role="button" class="btn btn-konfirmasi-persembahan">Konfirmasi<br/><?php echo get_the_title($idPersembahan); ?></a></div>
                 </div>
             </div>
             <?php }else{ ?>
@@ -42,7 +45,7 @@
                     <div class="col-12 offset-lg-5 col-lg-7">
                         <h4 class="title-single-persembahan text-center mb-3"><?php echo get_the_title($idPersembahan); ?></h4>
                         <div class="desc-single-persembahan text-white text-center mb-3"><?php echo get_the_content($idPersembahan); ?></div>
-                        <div class="text-center"><a href="" role="button" class="btn btn-konfirmasi-persembahan">Konfirmasi<br/><?php echo get_the_title($idPersembahan); ?></a></div>
+                        <div class="text-center"><a href="https://wa.me/<?php echo $wapersembahanconf; ?>" target="_blank" role="button" class="btn btn-konfirmasi-persembahan">Konfirmasi<br/><?php echo get_the_title($idPersembahan); ?></a></div>
                     </div>
                 </div>
             </div>
@@ -60,11 +63,14 @@
             $the_query = new WP_Query( $args );
             while ( $the_query->have_posts() ) : $the_query->the_post();
             $idPersembahan = get_the_ID();
+            $confirmation = get_post_meta($idPersembahan,'waconfirmation',true);
+            $wapersembahan = $confirmation[0]['wa-persembahan'];
+            $wapersembahanconf = $confirmation[0]['wa-persembahan-confirmation'];
 
             if($idPersembahan == 195){
         ?>
     <h4 class="title-single-persembahan text-dark text-center mb-3"><?php echo get_the_title($idPersembahan); ?></h4>
-    <div class="text-center"><a href="" role="button" class="btn btn-konfirmasi-persembahan w-25">Konfirmasi<br/>Dukungan Lainnya</a></div>
+    <div class="text-center"><a href="https://wa.me/<?php echo $wapersembahanconf; ?>" target="_blank" role="button" class="btn btn-konfirmasi-persembahan w-25">Konfirmasi<br/>Dukungan Lainnya</a></div>
     <?php } endwhile; 
     wp_reset_query(); ?>
 </div>
